26504ee248f10f09f70e7a113e12712604c45f56
travism26 <travismartin266@gmail.com>
2015-11-10 22:43:39 -0400

    update: global eventhandler added

---
+++ utils/src/main/java/src/main/java/GlobalAttributes/EventRaised.java
@@ -0,0 +1,17 @@
+package src.main.java.GlobalAttributes;
+
+/**
+ * Created by travismartin on 2015-11-10.
+ */
+public class EventRaised
+{
+    /*
+     * need to finish method
+     * Options:
+     * RaiseEvent(Model, SomeData, SomeData,...etc); well more complex but kinda secure?
+     * RaiseEvent(Model, IMandatoryData, IOptionalData); more extendable
+     */
+    public static void RaiseEvent(){
+
+    }
+}
